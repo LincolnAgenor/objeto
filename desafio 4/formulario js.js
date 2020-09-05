1d061946d514b9b0c3669e971b352ff0eee2fa19
LincolnAgenor <68159127+LincolnAgenor@users.noreply.github.com>
2020-09-05 13:16:49 -0300

    Add files via upload

---
+++ desafio 4/formulario js.js
@@ -0,0 +1,26 @@
+function form (){
+      
+    nome = document.getElementById("campo1");
+    RG = document.getElementById("campo2");
+    CPF = document.getElementById("campo3");
+    Email = document.getElementById("campo4");
+    Nota01 = document.getElementById("campo5");
+    Nota02 = document.getElementById("campo6");
+    Nota03 = document.getElementById("campo7");
+        
+}
+
+
+
+
+var aluno ={
+    nome:nome,
+    RG:RG,
+    CPF:CPF,
+    Email:Email,
+    Nota01:Nota01,
+    Nota02:Nota02,
+    Nota03:Nota03,
+    Media:Media,
+    situação:situação,
+}
---
+++ javascript  desafio objeto/javasript objeto.js
@@ -0,0 +1,34 @@
+function mostrarnome (){
+    var nome = document.querySelector(".js-input-name").value;
+    var RG = document.querySelector(".js-input-RG").value;
+    var CPF = document.querySelector(".js-input-CPF").value;
+    var Email = document.querySelector(".js-input-email").value;
+    var Nota01 = document.querySelector(".js-input-nota01").value;
+    var Nota02 = document.querySelector(".js-input-nota02").value;
+    var Nota03 = document.querySelector(".js-input-nota03").value;
+    var Media = document.querySelector(".js-input-media").value;
+    var situação = document.querySelector(".js-input-situção").value;
+
+    alert(nome);
+
+var aluno ={
+    nome:nome,
+    RG:RG,
+    CPF:CPF,
+    Email:Email,
+    Nota01:Nota01,
+    Nota02:Nota02,
+    Nota03:Nota03,
+    Media:Media,
+    situação:situação,
+
+    
+}
+};
+  
+    
+
+
+
+
+
